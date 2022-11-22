9179f19c4c847c95c66ca38b4e321090e0655a93
praveen <ppraveen5974@gmail.com>
2022-11-22 23:16:37 +0530

    new

--- setup.sh
+++ setup.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-# Copyright (c) 2021 Umegbewe Nwebedu
-
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 
 
 ## ANSI colors (FG & BG)
@@ -64,7 +43,7 @@ echo "
 		${ORANGE}                       |_|                              
 		${ORANGE}                ${RED}Version : 1.0
 
-		${GREEN}[${WHITE}-${GREENS}]${GREENS} Tool Created by great ${WHITE}
+		${GREEN}[${WHITE}-${GREENS}]${GREENS} Tool Created by parthanaboina praveen ${WHITE}
 "
 }
 
@@ -73,14 +52,12 @@ menu() {
 	{ clear; header; echo; }
 	cat <<- EOF
 		${RED}[${WHITE}::${RED}]${ORANGE} Select a tool to install ${RED}[${WHITE}::${RED}]${ORANGE}
-
-		${RED}[${WHITE}01${RED}]${ORANGE} Docker        ${RED}[${WHITE}08${RED}]${ORANGE} AWS Cli   	${RED}[${WHITE}15${RED}]${ORANGE} Nomad
-		${RED}[${WHITE}02${RED}]${ORANGE} Vagrant       ${RED}[${WHITE}09${RED}]${ORANGE} Gcloud Cli      ${RED}[${WHITE}16${RED}]${ORANGE} Ngrok
-		${RED}[${WHITE}03${RED}]${ORANGE} Ansible       ${RED}[${WHITE}10${RED}]${ORANGE} Azure Cli       ${RED}[${WHITE}17${RED}]${ORANGE} Helm
-		${RED}[${WHITE}04${RED}]${ORANGE} Terraform	   ${RED}[${WHITE}11${RED}]${ORANGE} Circleci Cli    ${RED}[${WHITE}18${RED}]${ORANGE} Terragrunt
-		${RED}[${WHITE}05${RED}]${ORANGE} Kubectl	   ${RED}[${WHITE}12${RED}]${ORANGE} Github Cli 	${RED}[${WHITE}19${RED}]${ORANGE} Kubeadm
-		${RED}[${WHITE}06${RED}]${ORANGE} Minikube      ${RED}[${WHITE}13${RED}]${ORANGE} Packer		${RED}[${WHITE}20${RED}]${ORANGE} Kubelet
-		${RED}[${WHITE}07${RED}]${ORANGE} Kind	   ${RED}[${WHITE}14${RED}]${ORANGE} Waypoint	${RED}[${WHITE}21${RED}]${ORANGE} Krew
+                                                                                                                          
+		${RED}[${WHITE}01${RED}]${ORANGE} Docker       ${RED}[${WHITE}05${RED}]${ORANGE} AWS Cli  	${RED}[${WHITE}09${RED}]${ORANGE} GclouldCli
+		${RED}[${WHITE}02${RED}]${ORANGE} Ansible      ${RED}[${WHITE}06${RED}]${ORANGE} Azure Cli  	${RED}[${WHITE}10${RED}]${ORANGE} Helm
+		${RED}[${WHITE}03${RED}]${ORANGE} Terraform    ${RED}[${WHITE}07${RED}]${ORANGE} Kubectl  	${RED}[${WHITE}11${RED}]${ORANGE} Github Cli
+		${RED}[${WHITE}04${RED}]${ORANGE} Kubeadm      ${RED}[${WHITE}08${RED}]${ORANGE} Minikube  	${RED}[${WHITE}12${RED}]${ORANGE} Kubelet
+		 	
 		
 		${RED}[${WHITE}q${RED}]${ORANGE} Exit
 
@@ -89,46 +66,33 @@ menu() {
 	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
 		if [[ "$REPLY" == 1 || "$REPLY" == 01 ]]; then
 			docker --version && echo ${RED} "Docker already installed" && sleep 2 && menu || dockerin
+
 		elif [[ "$REPLY" == 2 || "$REPLY" == 02 ]]; then
-			vagrant --version && echo ${RED} "Vagrant already installed" && sleep 2 && menu || vagrantin
-		elif [[ "$REPLY" == 3 || "$REPLY" == 03 ]]; then
 			ansible --version && echo ${RED} "Ansible already installed" && sleep 2 && menu || ansiblein
-		elif [[ "$REPLY" == 4 || "$REPLY" == 04 ]]; then
+		elif [[ "$REPLY" == 3 || "$REPLY" == 03 ]]; then
 			terraform --version && echo ${RED} "Terraform already installed" && sleep 2 && menu || terraformin
-		elif [[ "$REPLY" == 5 || "$REPLY" == 05 ]]; then
+		elif [[ "$REPLY" == 4 || "$REPLY" == 07 ]]; then
 			kubectl version --short && echo ${RED} "Kubectl already installed" && sleep 2 && menu || kubectlin
-		elif [[ "$REPLY" == 6 || "$REPLY" == 06 ]]; then
+		elif [[ "$REPLY" == 5 || "$REPLY" == 08 ]]; then
 			minikube version --short && echo ${RED} "Minikube already installed" && sleep 2 && menu || minikubein
-		elif [[ "$REPLY" == 7 || "$REPLY" == 07 ]]; then
-			kind --version && echo ${RED} "Kind already installed" && sleep 2 && menu || kindin
-		elif [[ "$REPLY" == 8 || "$REPLY" == 08 ]]; then
+		
+		elif [[ "$REPLY" == 6 || "$REPLY" == 05 ]]; then
 			aws --version && echo ${RED} "AWS Cli already installed" && sleep 2 && menu || awsclin
-		elif [[ "$REPLY" == 9 || "$REPLY" == 09 ]]; then
+		elif [[ "$REPLY" == 7 || "$REPLY" == 09 ]]; then
 			gcloud --version && echo ${RED} "Gloud Cli already installed" && sleep 2 && menu || gloudsdkin
-		elif [[ "$REPLY" == 10 || "$REPLY" == 010 ]]; then
+		elif [[ "$REPLY" == 8 || "$REPLY" == 06 ]]; then
 			az --version && echo ${RED} "Azure Cli already installed" && sleep 2 && menu || azureclin
-		elif [[ "$REPLY" == 11 || "$REPLY" == 011 ]]; then
-			circleci version && echo ${RED} "CircleCI Cli already installed" && sleep 2 && menu || circleclin
-		elif [[ "$REPLY" == 12 || "$REPLY" == 012 ]]; then
+
+		elif [[ "$REPLY" == 9 || "$REPLY" == 11 ]]; then
 			gh --version && echo ${RED} "Github Cli already installed" && sleep 2 && menu || githubclin
-		elif [[ "$REPLY" == 13 || "$REPLY" == 013 ]]; then
-			packer --version && echo ${RED} "Packer already installed" && sleep 2 && menu || packerin
-		elif [[ "$REPLY" == 14 || "$REPLY" == 014 ]]; then
-			waypoint --version && echo ${RED} "Waypoint already installed" && sleep 2 && menu || waypointin
-		elif [[ "$REPLY" == 15 || "$REPLY" == 015 ]]; then
-			nomad --version && echo ${RED} "Nomad already installed" && sleep 2 && menu || nomadin
-		elif [[ "$REPLY" == 16 || "$REPLY" == 016 ]]; then
-			ngrok --version && echo ${RED} "Ngrok already installed" && sleep 2 && menu || ngrokin
-		elif [[ "$REPLY" == 17 || "$REPLY" == 017 ]]; then
+		
+		
 			helm version && echo ${RED} "Helm already installed" && sleep 2 && menu || helmin
-		elif [[ "$REPLY" == 18 || "$REPLY" == 018 ]]; then
-			terragrunt -v && echo ${RED} "Terragrunt already installed" && sleep 2 && menu || terragruntin
-		elif [[ "$REPLY" == 19 || "$REPLY" == 019 ]]; then
+		elif [[ "$REPLY" == 10 || "$REPLY" == 04 ]]; then
 			kubeadm version && echo ${RED} "Kubeadm already installed" && sleep 2 && menu || kubeadmin
-		elif [[ "$REPLY" == 20 || "$REPLY" == 020 ]]; then
+		elif [[ "$REPLY" == 11 || "$REPLY" == 12 ]]; then
 			kubelet version && echo ${RED} "Kubelet already installed" && sleep 2 && menu || kubeletin
-		elif [[ "$REPLY" == 21 || "$REPLY" == 021 ]]; then
-			kubectl krew version | sed '2q;d' && echo ${RED} "Krew already installed" && sleep 2 && menu || krewin
+		
 		elif [[ "$REPLY" == q ]]; then
 			clear && exit
 		else
@@ -153,7 +117,7 @@ function dockerin {
 	sudo apt-get update && sudo apt install -y docker-ce docker-ce-cli containerd.io && echo ${RED} "Docker installed!!!"
 	sleep 3
 	menu
-	elif [[ `cat /etc/os-release | grep 'Rhel\|ID=centos\|ID_LIKE=rhel'` ]]; then
+	elif [[ `cat /etc/os-release | grep 'ID=rhel\|ID_LIKE=fedora'` ]]; then
 	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Yum detected installing docker.........."
 	sudo yum remove docker \
              	  docker-client \
@@ -174,35 +138,6 @@ function dockerin {
 	fi
 	}
 
-function vagrantin {
-	if [[ `cat /etc/os-release | grep 'Ubuntu\|ID_LIKE=ubuntu\|Debian\|ID_LIKE=debian'` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Ubuntu/Debian based detected installing Vagrant.........."
-	sleep 1
-	curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-	sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" && \
-	sudo apt-get update && sudo apt-get install vagrant && echo ${RED} "Vagrant installed!!!"
-	sleep 3
-	menu
-	elif [[ `cat /etc/os-release | grep 'Rhel\|ID_LIKE=centos\|Centos\|ID_LIKE=rhel'` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Yum detected installing Vagrant.........."
-	sleep 1
-	sudo yum install -y yum-utils && \
-	sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo && \
-	sudo yum -y install vagrant && echo ${RED} "Vagrant installed!!!"
-	sleep 3
-	menu
-	elif [[ `cat /etc/os-release | grep 'ID="amzn"'` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Yum detected installing Vagrant.........."
-	sleep 1
-	sudo yum install -y yum-utils && \
-	sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo && \
-	sudo yum -y install vagrant && echo ${RED} "Vagrant installed!!!"
-	sleep 3
-	menu
-	else
-	echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported package manager" && sleep 2 && menu;
-	fi
-}
 
 function ansiblein {
 	if [[ `cat /etc/os-release | grep 'Ubuntu\|ID_LIKE=ubuntu'` ]]; then
@@ -236,7 +171,7 @@ function terraformin {
 	sudo apt-get update && sudo apt-get install terraform && echo ${RED} "Terraform installed!!!"
 	sleep 3
 	menu
-	elif [[ `cat /etc/os-release | grep 'Rhel\|ID_LIKE=centos\|Centos\|ID_LIKE=rhel'` ]]; then
+	elif [[ `cat /etc/os-release | grep 'Rhel\|ID_LIKE="fedora"\|Centos\|ID_LIKE=rhel'` ]]; then
 	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Yum detected installing Vagrant.........."
 	sleep 1
 	sudo yum install -y yum-utils && \
@@ -307,25 +242,7 @@ function kubectlin {
 	fi
 }
 
-function krewin {
-	cd "$(mktemp -d)" &&
-      OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-      ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-      KREW="krew-${OS}_${ARCH}" &&
-      curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-      tar zxvf "${KREW}.tar.gz" &&
-      ./"${KREW}" install krew
 
-      export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
-
-      if [ -f "~/.bashrc" ]; then
-        echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> ~/.bashrc
-      fi
-
-      if [ -f "~/.zshrc" ]; then
-        echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> ~/.zshrc
-      fi
-}
 
 function minikubein {
 	if [[ `cat /etc/os-release | grep 'Ubuntu\|ID_LIKE=ubuntu\|Debian\|ID_LIKE=debian'` ]]; then
@@ -340,19 +257,7 @@ function minikubein {
 	fi
 }
 
-function kindin {
-	if [[ `uname -a | grep "Linux"` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Linux detected installing Kind.........."
-	sleep 1
-	curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64 && \
-	sudo chmod +x ./kind && \
-	sudo mv ./kind /usr/bin/kind && echo ${RED} "Kind installed!!!"
-	sleep 3
-	menu
-	else
-	echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported operating system" && sleep 2 && menu;
-	fi
-}
+
 
 function awsclin {
 	if [[ `uname -a | grep "Linux"` ]]; then
@@ -408,16 +313,6 @@ function azureclin {
 	fi
 }
 
-function circleclin {
-	if [[ `uname -a | grep "Linux"` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Linux OS detected installing Cirlceci Cli.........."
-	curl -fLSs https://raw.githubusercontent.com/CircleCI-Public/circleci-cli/master/install.sh | sudo bash && echo ${RED} "CircleCI Cli installed!!!"
-	sleep 3
-	menu
-	else
-	echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported operating system" && sleep 2 && menu;
-	fi
-}
 
 function githubclin {
 	if [[ `cat /etc/os-release | grep 'Ubuntu\|ID_LIKE=ubuntu\|Debian\|ID_LIKE=debian'` ]]; then
@@ -431,107 +326,6 @@ function githubclin {
 	echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported operating system" && sleep 2 && menu;
 	fi
 
-}
-
-function packerin {
-	if [[ `cat /etc/os-release | grep 'Ubuntu\|ID_LIKE=ubuntu\|Debian\|ID_LIKE=debian'` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Ubuntu/Debian based detected installing Packer.........."
-	curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - && \
-	sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" && \
-	sudo apt-get update && sudo apt-get install packer && echo ${RED} "Packer installed!!!"
-	sleep 3
-	menu
-	elif [[ `cat /etc/os-release | grep 'Rhel\|ID_LIKE=centos\|Centos\|ID_LIKE=rhel'` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Yum detected installing Packer.........."
-	sleep 1
-	sudo yum install -y yum-utils && \
-	sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo && \
-	sudo yum -y install packer && echo ${RED} "Packer installed!!!"
-	sleep 3
-	menu
-	elif [[ `cat /etc/os-release | grep 'ID="amzn"'` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Yum detected installing Packer.........."
-	sleep 1
-	sudo yum install -y yum-utils && \
-	sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo && \
-	sudo yum -y install packer && echo ${RED} "Packer installed!!!"
-	sleep 3
-	menu
-	else
-	echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported operating system" && sleep 2 && menu;
-	fi
-	
-}
-
-function waypointin {
-	if [[ `cat /etc/os-release | grep 'Ubuntu\|ID_LIKE=ubuntu\|Debian\|ID_LIKE=debian'` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Ubuntu/Debian based detected installing Waypoint.........."
-	curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - && \
-	sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" && \
-	sudo apt-get update && sudo apt-get install waypoint && echo ${RED} "Waypoint installed!!!"
-	sleep 3
-	menu
-	elif [[ `cat /etc/os-release | grep 'Rhel\|ID_LIKE=centos\|Centos\|ID_LIKE=rhel'` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Yum detected installing Waypoint.........."
-	sleep 1
-	sudo yum install -y yum-utils && \
-	sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo && \
-	sudo yum -y install waypoint && echo ${RED} "Waypoint installed!!!"
-	sleep 3
-	menu
-	elif [[ `cat /etc/os-release | grep 'ID="amzn"'` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Yum detected installing Waypoint.........."
-	sleep 1
-	sudo yum install -y yum-utils && \
-	sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo && \
-	sudo yum -y install waypoint && echo ${RED} "Waypoint installed!!!"
-	sleep 3
-	menu
-	else
-	echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported operating system" && sleep 2 && menu;
-	fi
-}
-
-function nomadin {
-	if [[ `cat /etc/os-release | grep 'Ubuntu\|ID_LIKE=ubuntu\|Debian\|ID_LIKE=debian'` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Ubuntu/Debian based detected installing Nomad........."
-	curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - && \
-	sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" && \
-	sudo apt-get update && sudo apt-get install nomad && echo ${RED} "Nomad installed!!!"
-	sleep 3
-	menu
-	elif [[ `cat /etc/os-release | grep 'Rhel\|ID_LIKE=centos\|Centos\|ID_LIKE=rhel'` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Yum detected installing Nomad.........."
-	sleep 1
-	sudo yum install -y yum-utils && \
-	sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo && \
-	sudo yum -y install nomad && echo ${RED} "Nomad installed!!!"
-	sleep 3
-	menu
-	elif [[ `cat /etc/os-release | grep 'ID="amzn"'` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Yum detected installing Nomad.........."
-	sleep 1
-	sudo yum install -y yum-utils && \
-	sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo && \
-	sudo yum -y install nomad && echo ${RED} "Nomad installed!!!"
-	sleep 3
-	menu
-	else
-	echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported operating system" && sleep 2 && menu;
-	fi
-}
-
-function ngrokin {
-	if [[ `uname -a | grep "Linux"` ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Linux OS detected installing Ngrok.........."
-	sudo apt-get install unzip && wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -o ngrok.zip && \
-	unzip ngrok.zip && \
-	sudo mv ./ngrok /usr/bin/ngrok && echo ${RED} "Ngrok installed!!!"
-	sleep 3
-	menu
-	else
-	echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported operating system" && sleep 2 && menu;
-	fi
 }
 
 function helmin {
